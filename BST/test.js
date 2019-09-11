ebe211de683e5d638b9fc3eefc5d6c01ab48dd5e
Kuan Xing <kxing@salesforce.com>
2019-09-11 15:08:08 -0700

    109

--- BST/test.js
+++ BST/test.js
@@ -22,3 +22,45 @@ var sortedArrayToBST = function(nums) {
         return node;
     }
 };
+
+
+// 109. Convert Sorted List to Binary Search Tree
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    var stack = [];
+    while(head != null) {
+        stack.push(head.val);
+        head = head.next;
+    }
+
+    var sortedArrayToBST = function(nums) {
+        if(nums.length==0){
+            return null;
+        } else {
+            var m = Math.floor(nums.length/2);
+            var mid = nums[ m ];
+            var node = new TreeNode(mid);
+            node.left = sortedArrayToBST(nums.slice(0,m));
+            node.right = sortedArrayToBST(nums.slice(m+1));
+            return node;
+        }
+    };
+    return sortedArrayToBST(stack);
+};
